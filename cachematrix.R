f6629bbc4a26f9c44b56e0333c0fb9ab96b3ae3f
Flemming Stark <flemming@starkware.co>
2014-04-27 20:50:27 -0400

    adding code to cachematrix functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,41 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+##this code creates two functions to enable the user to have a spacial matrix object
+##that can contain the inverse value and another function to compute the inverse
+##or return the one already cached in previous computes with the same values in the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+##This function create an special matrix taking a matrix objext as input
+    inverse <- NULL
+    set <- function(y) {
+        x <<- y
+        ##this set the inverse to NULL in every change to make sure the cached value
+        ##is not from a different value than the current one
+        inverse <<- NULL
+    }
+    get <- function() x
+    ##this make it able to store the value and the inverse in the same object
+    setinverse <- function(value) inverse <<- value
+    getinverse <- function() inverse
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+##This function receive a matrix object and return its inverse
+
+    ##first checking if it was not computed before and the matrix have not changed
+    inverse <- x$getinverse()
+    if(!is.null(inverse)) {
+        message("getting cached inverse")
+        #and returning the cached value if it was
+        return(inverse)
+    }
+    data <- x$get()
+    inverse <- solve(data, ...)
+    x$setinverse(inverse)
+    inverse
+
 }
